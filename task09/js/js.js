d1f46f3fa13074e144fced34971586357d1a31c3
Mikhail <newlevelcenter@gmail.com>
2022-10-14 15:41:52 +0300

    loops

---
+++ task09/js/js.js
@@ -0,0 +1,17 @@
+let a = 1;
+let b = 456;
+let c = '1234';
+console.log(a);
+console.log(b);
+console.log(c);
+
+let arr = [1,2,3,5]; //Ваші оцінки за екзамени в останню сесію.
+console.log(arr[0]);
+
+for (let i = 0;i<5;i=i+1){
+    console.log(i);
+}
+
+for (let i = 0;i<arr.length;i=i+1){
+    console.log(arr[i]);
+}
